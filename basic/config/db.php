50f14a9deaf8afd4848b8b2c9e6afc8c738a4563
dconstancio <diego.constancio@gmail.com>
2015-11-03 12:38:24 -0200

    031115
    031115

--- basic/config/db.php
+++ basic/config/db.php
@@ -5,8 +5,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mydb',
+    'dsn' => 'mysql:host=localhost;dbname=ibg',
     'username' => 'root',
-    'password' => '',
+    'password' => 'dsc',
     'charset' => 'utf8',
 ];
